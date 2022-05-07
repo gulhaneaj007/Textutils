e558f6f53af20ed46920e3b7ddc7971bf3bc9afd
gulhaneaj007 <88966594+gulhaneaj007@users.noreply.github.com>
2022-05-07 06:21:03 +0530

    Creating "Enable Dark Mode" Button Using useState Hook | Complete React Course in Hindi #10

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
+// import About from './components/About';
+
 function App() {
   return (
 <>   
     <Navbar title="TextUtils" AboutText="About US"/>
     <div className='container my 3'>
       <Textform  heading="Enter text to analyse below"  />
+      {/* <About/> */}
     </div>
 
 </>
