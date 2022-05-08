765c2bde6b093e0837c2548d8cefeb68b7664574
gulhaneaj007 <88966594+gulhaneaj007@users.noreply.github.com>
2022-05-08 23:10:58 +0530

    Exercise 1: Solutions + Shoutouts | Complete React Course in Hindi #11

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -4,23 +4,33 @@ export default function Textform(props) {
     const [text,setText]=useState('')
 
     const changeUPcase=()=>{
-        // console.log("click") 
         let nextext=text.toUpperCase();
 
         setText(nextext )
     }
     const changeLowercase=()=>{
-      // console.log("click") 
       let nextext=text.toLowerCase();
 
       setText(nextext )
   }
 
     const handleOnchange=(event)=>{
-        // console.log("Done ")
         setText(event.target.value);
 
     }
+
+    const handleCopy=()=>{
+      var text = document.getElementById('myBox')
+      text.select()
+      navigator.clipboard.writeText(text.value)
+    }
+
+    const handleSpace=()=>{
+      var Newtext = text.split(/[ ]+/);
+      setText(Newtext.join(' '))
+    }
+
+
     return (
       <>
     <div className='container'>
@@ -30,6 +40,10 @@ export default function Textform(props) {
         </div>
         <button type="button" className="btn btn-primary mx-1" onClick={changeUPcase}>Convert to UpperCase</button>
         <button type="button" className="btn btn-primary mx-1" onClick={changeLowercase}>Lowercase</button>
+        <button type="button" className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
+        <button type="button" className="btn btn-primary mx-1" onClick={handleSpace}>Remove Extra space</button>
+
+
 
     </div>
     <div class="container my-3">
--- src/components/dummy2.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function dummy2() {
-  return (
-    <div>
-      <p>This dummy 2</p>
-    </div>
-  )
-}
